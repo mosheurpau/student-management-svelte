981cb774ce436faa9ae426681635800418e0ab03
mosheurpau <mosheur.pau@gamil.com>
2024-06-20 08:08:56 +0600

    added student name, age update option

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -10,9 +10,14 @@
   function deleteStudent(event) {
     students = students.filter((student) => student.id !== event.detail.id);
   }
+  function updateStudent(event) {
+    students = students.map((student) =>
+      student.id === event.detail.id ? { ...student, ...event.detail } : student
+    );
+  }
 </script>
 
 <div class="min-h-screen">
   <AddStudent on:add={addStudent} />
-  <StudentList {students} on:delete={deleteStudent} />
+  <StudentList {students} on:delete={deleteStudent} on:update={updateStudent} />
 </div>
--- src/components/StudentList.svelte
+++ src/components/StudentList.svelte
@@ -3,6 +3,29 @@
 
   export let students = [];
   const dispatch = createEventDispatcher();
+
+  let editingId = null;
+  let editingName = "";
+  let editingAge = "";
+
+  function startEditing(student) {
+    editingId = student.id;
+    editingName = student.name;
+    editingAge = student.age;
+  }
+
+  function saveEdit() {
+    dispatch("update", { id: editingId, name: editingName, age: editingAge });
+    editingId = null;
+    editingName = "";
+    editingAge = "";
+  }
+
+  function cancelEdit() {
+    editingId = null;
+    editingName = "";
+    editingAge = "";
+  }
 </script>
 
 <table class="min-w-full bg-white">
@@ -15,18 +38,57 @@
   </thead>
   <tbody>
     {#each students as student}
-      <tr>
-        <td class="py-2 px-4 border-b">{student.name}</td>
-        <td class="py-2 px-4 border-b">{student.age}</td>
-        <td class="py-2 px-4 border-b">
-          <button
-            class="bg-red-500 text-white py-1 px-2 rounded"
-            on:click={() => dispatch("delete", { id: student.id })}
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
+      {#if editingId === student.id}
+        <tr>
+          <td class="py-2 px-4 border-b">
+            <input
+              type="text"
+              bind:value={editingName}
+              class="border py-1 px-2 w-full"
+            />
+          </td>
+          <td class="py-2 px-4 border-b">
+            <input
+              type="number"
+              bind:value={editingAge}
+              class="border py-1 px-2 w-full"
+            />
+          </td>
+          <td class="py-2 px-4 border-b">
+            <button
+              class="bg-green-500 text-white py-1 px-2 rounded mr-2"
+              on:click={saveEdit}
+            >
+              Save
+            </button>
+            <button
+              class="bg-gray-500 text-white py-1 px-2 rounded"
+              on:click={cancelEdit}
+            >
+              Cancel
+            </button>
+          </td>
+        </tr>
+      {:else}
+        <tr>
+          <td class="py-2 px-4 border-b">{student.name}</td>
+          <td class="py-2 px-4 border-b">{student.age}</td>
+          <td class="py-2 px-4 border-b">
+            <button
+              class="bg-blue-500 text-white py-1 px-2 rounded mr-2"
+              on:click={() => startEditing(student)}
+            >
+              Update
+            </button>
+            <button
+              class="bg-red-500 text-white py-1 px-2 rounded"
+              on:click={() => dispatch("delete", { id: student.id })}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      {/if}
     {/each}
   </tbody>
 </table>
