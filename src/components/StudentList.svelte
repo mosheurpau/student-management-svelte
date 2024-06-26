a4127747a8c06e1992c87b52e0a010d32b98aadc
mosheurpau <mosheur.pau@gamil.com>
2024-06-26 17:41:07 +0600

    added some style

--- src/components/StudentList.svelte
+++ src/components/StudentList.svelte
@@ -13,45 +13,49 @@
   }
 </script>
 
-<table class="min-w-full bg-white">
-  <thead>
-    <tr>
-      <th class="py-2 px-4 bg-gray-300 text-left">Name</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">Gender</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">Age</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">Country</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">State</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">City</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">Edit</th>
-      <th class="py-2 px-4 bg-gray-300 text-left">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each students as student}
+<div class="overflow-x-auto">
+  <table class="table table-xs table-pin-rows table-pin-cols">
+    <thead>
       <tr>
-        <td class="py-2 px-4 border-b">{student.name}</td>
-        <td class="py-2 px-4 border-b">{student.gender}</td>
-        <td class="py-2 px-4 border-b">{student.age}</td>
-        <td class="py-2 px-4 border-b">{student.country}</td>
-        <td class="py-2 px-4 border-b">{student.state}</td>
-        <td class="py-2 px-4 border-b">{student.city}</td>
-        <td class="py-2 px-4 border-b">
-          <button
-            class="bg-green-500 text-white py-1 px-2 rounded"
-            on:click={() => editStudent(student)}
-          >
-            Edit
-          </button>
-        </td>
-        <td class="py-2 px-4 border-b">
-          <button
-            class="bg-red-500 text-white py-1 px-2 rounded"
-            on:click={() => deleteStudent(student.id)}
-          >
-            Delete
-          </button>
-        </td>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Index</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Name</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Gender</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Age</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Country</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">State</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">City</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Edit</th>
+        <th class="py-2 px-4 bg-gray-300 text-black text-left">Delete</th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each students as student, Index}
+        <tr>
+          <td class="py-2 px-4 border-b">{Index + 1}</td>
+          <td class="py-2 px-4 border-b">{student.name}</td>
+          <td class="py-2 px-4 border-b">{student.gender}</td>
+          <td class="py-2 px-4 border-b">{student.age}</td>
+          <td class="py-2 px-4 border-b">{student.country}</td>
+          <td class="py-2 px-4 border-b">{student.state}</td>
+          <td class="py-2 px-4 border-b">{student.city}</td>
+          <td class="py-2 px-4 border-b">
+            <button
+              class="bg-green-500 text-white py-1 px-2 rounded"
+              on:click={() => editStudent(student)}
+            >
+              Edit
+            </button>
+          </td>
+          <td class="py-2 px-4 border-b">
+            <button
+              class="bg-red-500 text-white py-1 px-2 rounded"
+              on:click={() => deleteStudent(student.id)}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
