50e09e9fde8df01a59724988ebfa3b60689866fa
mosheurpau <mosheur.pau@gamil.com>
2024-07-08 17:42:06 +0600

    Added build file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,6 @@
   import "../app.css";
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
-  import { createClient, setContextClient } from "@urql/svelte";
-
-  export const load = async () => {
-    const client = createClient({
-      url: "hhttp://localhost:4000", // Ensure the correct URL endpoint
-    });
-
-    setContextClient(client);
-  };
 </script>
 
 <Header />
