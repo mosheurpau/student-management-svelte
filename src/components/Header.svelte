29c3ce45c4538a661131ea89cff925d4d6a4c68f
mosheurpau <mosheur.pau@gamil.com>
2024-07-09 17:35:03 +0600

    added some icon and organize side navbar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,6 +2,22 @@
   import { authStore } from "$lib/stores/authStore";
   import { get } from "svelte/store";
   import { page } from "$app/stores";
+  import {
+    ArrowRight,
+    ArrowRightCircle,
+    ArrowUp,
+    BellAlert,
+    CheckBadge,
+    ChevronRight,
+    Cog6Tooth,
+    FingerPrint,
+    Home,
+    Icon,
+    MagnifyingGlass,
+    ShieldCheck,
+    Sun,
+    XCircle,
+  } from "svelte-hero-icons";
 
   let authState;
 
@@ -52,6 +68,20 @@
           </svg>
         </label>
       </div>
+      <div class="form-control">
+        <div class="relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            class="input input-bordered w-24 md:w-auto pl-10 focus:border-green-500"
+          />
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
+            <Icon src={MagnifyingGlass} size="16" />
+          </div>
+        </div>
+      </div>
     </div>
 
     <!-- Drawer (sidebar) -->
@@ -69,61 +99,118 @@
         <ul class="menu bg-base-200 text-base-content min-h-full w-52 p-4">
           <!-- Sidebar content here -->
           <li>
-            <p class="md:text-2xl text-sm font-bold text-green-500">
+            <p
+              class="md:text-2xl text-sm font-bold text-green-500 hover:cursor-text"
+            >
               Student Management
             </p>
           </li>
           <li>
-            <a href="/" class="block px-4 py-2 hover:text-green-500">Home</a>
-          </li>
-          <li>
-            <a href="/protected" class="block px-4 py-2 hover:text-green-500"
-              >Protected</a
+            <a href="/" class="px-4 py-2 hover:text-green-500 inline-flex"
+              ><span><Icon src={Home} size="16" /></span> Home</a
             >
           </li>
           <li>
-            <a href="/dashboard" class="block px-4 py-2 hover:text-green-500"
-              >Dashboard</a
+            <a
+              href="/protected"
+              class="px-4 py-2 hover:text-green-500 inline-flex"
+              ><span><Icon src={FingerPrint} size="16" /></span> Protected</a
             >
+          </li>
+          <li>
+            <a
+              href="/dashboard"
+              class="px-4 py-2 hover:text-green-500 inline-flex"
+              ><span><Icon src={CheckBadge} size="16" /></span>Dashboard</a
+            >
+          </li>
+          <li class="dropdown dropdown-bottom">
+            <div tabindex="0" role="button" class="btn">
+              <span><Icon src={ShieldCheck} size="16" /></span>
+              <span>Authentication</span>
+              <span><Icon class="mt-1" src={ChevronRight} size="16" /></span>
+            </div>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <ul tabindex="0" class="dropdown-content bg-transparent">
+              <li>
+                <a
+                  href="/login"
+                  class="px-4 py-2 hover:text-green-500 inline-flex"
+                  ><span><Icon src={ArrowRightCircle} size="16" /></span
+                  >Login</a
+                >
+              </li>
+              <li>
+                <a
+                  href="/register"
+                  class="px-4 py-2 hover:text-green-500 inline-flex"
+                  ><span><Icon src={XCircle} size="16" /></span>Register</a
+                >
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
     </div>
 
     <!-- Center content (for large screens) -->
-    <div class="navbar-center hidden lg:flex">
-      <p class="md:text-2xl text-sm font-bold text-green-500 mr-96">
-        Student Management
-      </p>
+    <div class="navbar-center hidden lg:flex z-10">
       <ul class="menu menu-horizontal px-1">
-        {#each tabs as tab}
-          <li>
-            <a
-              href={tab.link}
-              class={` hover:text-green-500 ${currentPath === tab.link ? "active" : ""} px-4 py-2`}
-              >{tab.name}</a
-            >
-          </li>
-        {/each}
+        <li>
+          <span class="hover:text-green-500"><Icon src={Sun} size="26" /></span>
+        </li>
+        <li>
+          <span class="hover:text-green-500"
+            ><Icon src={BellAlert} size="26" /></span
+          >
+        </li>
+        <li>
+          <span class="hover:text-green-500"
+            ><Icon src={Cog6Tooth} size="26" /></span
+          >
+        </li>
       </ul>
-    </div>
-
-    <!-- Right content (authentication) -->
-    <div class="navbar-end flex items-center space-x-4">
-      {#if authState.isAuthenticated}
-        <p class="inline-block">Welcome, {authState.user}</p>
-        <button
-          on:click={handleLogout}
-          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >Logout</button
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
+          </div>
+        </div>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-4 shadow"
         >
-      {:else}
-        <a
-          href="/login"
-          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          >Login</a
-        >
-      {/if}
+          <li>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a class="justify-between">
+              Profile
+              <span class="badge">New</span>
+            </a>
+          </li>
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><a>Settings</a></li>
+          <li>
+            {#if authState.isAuthenticated}
+              <p class="inline-block">Welcome, {authState.user}</p>
+              <button
+                on:click={handleLogout}
+                class=" bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded"
+                >Logout</button
+              >
+            {:else}
+              <a
+                href="/login"
+                class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 rounded"
+                >Login</a
+              >
+            {/if}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </div>
